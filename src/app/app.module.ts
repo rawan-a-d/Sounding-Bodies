4f4e4405bd17255e17f5c141ab63569b0fd8160f
Alayoubi,Ranim R <r.alayoubi@student.fontys.nl>
2021-06-17 14:09:28 +0200

    Switch statement is added
    Adding directives and routes still need to be done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ScrollEmotionsComponent } from './scroll-emotions/scroll-emotions.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BottomNavModule } from 'ngx-bottom-nav';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
@@ -13,7 +12,6 @@ import { DragscrollComponent } from './dragscroll/dragscroll.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ScrollEmotionsComponent,
     DragscrollComponent,
   ],
   imports: [
--- src/app/dragscroll/dragscroll.component.ts
+++ src/app/dragscroll/dragscroll.component.ts
@@ -25,6 +25,61 @@ export class DragscrollComponent implements OnInit {
   onIndexChanged(idx: number) { 
     this.index = idx;
     console.log('current index: ' + idx);
+    this.index = idx;
+
+    switch(idx) { 
+      case 0: { 
+         //statements; 
+         console.log("0 Pos: " + idx)
+         break; 
+      } 
+      case 1: { 
+         //statements; 
+         console.log("1 Pos: " + idx)
+         break; 
+      } 
+      case 2: { 
+        //statements; 
+        console.log("2 Pos: " + idx)
+        break; 
+      } 
+      case 3: { 
+        //statements; 
+        console.log("3 Pos: " + idx)
+        break; 
+      } 
+      case 4: { 
+        //statements; 
+        console.log("4 Pos: " + idx)
+        break; 
+      } 
+      default: { 
+         //statements; 
+         break; 
+      } 
+    }
   }
+
+  // test(idx: number){
+
+  //   this.index = idx;
+
+  //   switch(idx) { 
+  //     case 0: { 
+  //        //statements; 
+  //        console.log("0 Pos: " + idx)
+  //        break; 
+  //     } 
+  //     case 1: { 
+  //        //statements; 
+  //        console.log("1 Pos: " + idx)
+  //        break; 
+  //     } 
+  //     default: { 
+  //        //statements; 
+  //        break; 
+  //     } 
+  //  } 
+  // }
 
 }
